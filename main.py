7518db51809d99eeb70bd39ed8939573d877ef6c
gianlucagiudice <gianlu.giudice@gmail.com>
2024-04-13 23:31:44 +0200

    Inference

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import torch
 
-from yagpt.model import YaGPT
-from yagpt.utils import gpt_factory
+from yagpt.model import YaGPT, YaGPTConfig
 
 
 def main(
         batch_size: int = 8,
-        vocab_size: int = 32_000,
-        d_model: int = 768,
+        d_model: int = 256,
         seq_len: int = 512,
+        n_heads: int = 4,
+        n_layers: int = 6,
+        dropout: float = 0.1,
+        vocab_size: int = 32_000,
 ):
-    model: YaGPT = gpt_factory(
-        vocab_size=vocab_size,
+
+    config = YaGPTConfig(
+        seq_len=seq_len,
         d_model=d_model,
-        seq_len=seq_len
+        d_ff=d_model * 4,
+        n_heads=n_heads,
+        n_layers=n_layers,
+        dropout=dropout,
+        vocab_size=vocab_size
     )
+
+    model: YaGPT = YaGPT(config)
 
     input_ids = torch.randint(low=0, high=vocab_size, size=(batch_size, seq_len))
 
