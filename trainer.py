3c224a36b343457b70b810dafb4b290c60d9f750
gianlucagiudice <gianlu.giudice@gmail.com>
2024-05-04 16:53:30 +0200

    Hyperparameters

--- trainer.py
+++ trainer.py
@@ -10,44 +10,6 @@ from yagpt.dataset.divina_commedia import DivinaCommediaDataset, collate_fn
 from yagpt.model import YaGPT
 
 
-@torch.no_grad()
-def autoregressive_prediction(
-        model: torch.nn.Module,
-        validation_set: torch.utils.data.DataLoader,
-        device: torch.device,
-        idx2token: dict,
-        n_samples: int = 4,
-        autoregressive_steps: int = 64,
-):
-    model.eval()
-    model.to(device)
-
-    for i, batch in enumerate(validation_set):
-        generated_text = []
-        x, _ = batch
-        x = x.to(device)
-
-        x_original = x.clone()
-
-        for step in range(autoregressive_steps):
-            pred = model(x)
-
-            pred_id = torch.argmax(pred[0, -1, :]).item()
-            generated_text.append(idx2token[pred_id])
-
-            last_id = torch.tensor(pred_id).unsqueeze(dim=0).to(device)
-            x = torch.cat([x[0, 1:], last_id]).unsqueeze(dim=0)
-
-        print(f'\n\nORIGINAL >>>\n'
-              f'{''.join([idx2token[token_id] for token_id in x_original.flatten().tolist()])}'
-              f'\n'
-              f'GENERATED >>>\n'
-              f'{''.join(generated_text)}')
-
-        if i == n_samples:
-            break
-
-
 def main(
         dataset_path: str,
         batch_size: int,
@@ -57,7 +19,9 @@ def main(
         n_layers: int,
         train_ratio: float = 0.7,
         dropout: float = 0.1,
-        n_epochs: int = 100,
+        n_epochs: int = 50,
+        val_check_interval: int = 500,
+        limit_val_batches: int = 30,
 ):
     # Load datasets
     train_dataset = DivinaCommediaDataset(dataset_path, seq_len, 'train', train_ratio=train_ratio)
@@ -79,8 +43,8 @@ def main(
     trainer = L.Trainer(
         max_epochs=n_epochs,
         logger=L.pytorch.loggers.WandbLogger(project="YaGPT", log_model='all'),
-        val_check_interval=500,
-        limit_val_batches=30,
+        val_check_interval=val_check_interval,
+        limit_val_batches=limit_val_batches,
         callbacks=[
             L.pytorch.callbacks.ModelCheckpoint(save_last=True),
             L.pytorch.callbacks.EarlyStopping(monitor='val_loss', patience=5),
@@ -95,9 +59,9 @@ if __name__ == '__main__':
         dataset_path=dataset_path,
         batch_size=16,
         d_model=512,
-        seq_len=256,
-        n_heads=8,
-        n_layers=6,
+        seq_len=512,
+        n_heads=4,
+        n_layers=8,
         dropout=0.1,
-        train_ratio=0.7
+        train_ratio=0.7,
     )
