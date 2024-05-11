ca40ef90afc69d2a541414d48cc62fea36f50c89
gianlucagiudice <gianlu.giudice@gmail.com>
2024-05-11 21:11:25 +0200

    Update parameters

--- trainer.py
+++ trainer.py
@@ -2,17 +2,19 @@ from typing import Optional
 
 import fire
 import lightning
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
 from lightning.pytorch.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader
 
-from yagpt.utils import model_factory
 from yagpt.callback import TrainingGenerationCallback
 from yagpt.dataset import DivinaCommediaDataset, collate_fn
-from yagpt.model import YaGPTWrapper
+from yagpt.model import YaGPTWrapper, YaGPTConfig
 
 
 def main(
+        # Dataset
         dataset_path: str,
+        # Model parameters
         batch_size: int = 16,
         d_model: int = 512,
         seq_len: int = 192,
@@ -20,10 +22,17 @@ def main(
         n_layers: int = 12,
         train_ratio: float = 0.9,
         dropout: float = 0.1,
+        # Training parameters
         n_epochs: int = 10,
         val_check_interval: Optional[int] = None,
         limit_val_batches: Optional[int] = None,
         log_every_n_steps: int = 5,
+        gradient_clip_val: float = 1.0,
+        early_stopping_patience: int = 3,
+        # Optimizer parameters
+        lr: float = 1e-3,
+        scheduler_t0: int = 300,
+        scheduler_t_mult: int = 1,
 ):
     # Load datasets
     train_dataset = DivinaCommediaDataset(dataset_path, seq_len, 'train', train_ratio=train_ratio)
@@ -38,8 +47,17 @@ def main(
         shuffle=False, num_workers=8, persistent_workers=True
     )
 
-    vocab_size = train_dataset.vocab_size
-    model: YaGPTWrapper = model_factory(d_model, seq_len, n_heads, n_layers, dropout, vocab_size)
+    model_config = YaGPTConfig(
+        seq_len=seq_len,
+        d_model=d_model,
+        d_ff=d_model * 4,
+        n_heads=n_heads,
+        n_layers=n_layers,
+        dropout=dropout,
+        vocab_size=train_dataset.vocab_size
+    )
+
+    model = YaGPTWrapper(model_config, lr=lr, scheduler_t0=scheduler_t0, scheduler_t_mult=scheduler_t_mult)
 
     # Train model
     trainer = lightning.Trainer(
@@ -49,10 +67,13 @@ def main(
         val_check_interval=val_check_interval,
         limit_val_batches=limit_val_batches,
         callbacks=[
-            lightning.pytorch.callbacks.ModelCheckpoint(dirpath='checkpoints', monitor='val_loss', mode='min'),
+            ModelCheckpoint(dirpath='checkpoints', monitor='val_loss', mode='min'),
+            EarlyStopping(monitor='val_loss', patience=early_stopping_patience, mode='min'),
+            LearningRateMonitor(logging_interval='step'),
             TrainingGenerationCallback(n_samples=4, autoregressive_steps=32),
-            lightning.pytorch.callbacks.EarlyStopping(monitor='val_loss', patience=3),
         ],
+        gradient_clip_val=gradient_clip_val,
+        detect_anomaly=True
     )
     trainer.fit(model, train_loader, val_loader)
 
