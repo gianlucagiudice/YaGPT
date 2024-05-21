fdb5422236c91d572cfd483718f6aca1574d5379
gianlucagiudice <gianlu.giudice@gmail.com>
2024-05-21 22:17:43 +0200

    fix: Fix logging strategy

--- yagpt/callback/train_gen_callback.py
+++ yagpt/callback/train_gen_callback.py
@@ -1,5 +1,7 @@
+import enum
 from typing import List, Dict
 
+import wandb
 import lightning
 import torch
 from lightning.pytorch.callbacks import Callback
@@ -8,10 +10,29 @@ from torch.utils.data import DataLoader
 from yagpt.model import YaGPTWrapper
 
 
+class TableColumns(enum.Enum):
+    CONTEXT = 'context'
+    GENERATED = 'generated'
+    SPLIT = 'split'
+    STEP = 'step'
+    EPOCH = 'epoch'
+
+    @staticmethod
+    def get_values():
+        return [
+            TableColumns.CONTEXT.value,
+            TableColumns.GENERATED.value,
+            TableColumns.SPLIT.value,
+            TableColumns.STEP.value,
+            TableColumns.EPOCH.value
+        ]
+
+
 class TrainingGenerationCallback(Callback):
     def __init__(self, n_samples: int = 4, autoregressive_steps: int = 16):
         self.n_samples = n_samples
         self.autoregressive_steps = autoregressive_steps
+        self.table = wandb.Table(columns=TableColumns.get_values())
 
     @staticmethod
     def autoregressive_generation(
@@ -31,10 +52,10 @@ class TrainingGenerationCallback(Callback):
             x_sample, _ = random_val_dataloader.dataset[sample_id]
             x_sample = torch.tensor(x_sample).unsqueeze(dim=0)
 
-            generated_tokens = pl_module.model.generate_text(x_sample, autoregressive_steps)
+            tokens_iterator = pl_module.model.generate_text(x_sample, autoregressive_steps)
             # Decode the generated tokens
             context_text = [idx2token[token] for token in x_sample.flatten().tolist()]
-            generated_text = [idx2token[token] for token in generated_tokens.flatten().tolist()]
+            generated_text = [idx2token[token] for token in tokens_iterator]
 
             data.append({
                 'context': context_text,
@@ -60,20 +81,21 @@ class TrainingGenerationCallback(Callback):
 
             generated = [
                 {
-                    'context': dataloader.dataset.untokenize(x['context']),
-                    'generated': dataloader.dataset.untokenize(x['generated']),
-                    'split': split,
-                    'step': trainer.global_step,
-                    'epoch': trainer.current_epoch
+                    TableColumns.CONTEXT.value: dataloader.dataset.untokenize(x['context']),
+                    TableColumns.GENERATED.value: dataloader.dataset.untokenize(x['generated']),
+                    TableColumns.SPLIT.value: split,
+                    TableColumns.STEP.value: trainer.global_step,
+                    TableColumns.EPOCH.value: trainer.current_epoch
                 }
                 for x in autoregressive_generation
             ]
 
-            trainer.logger.log_table(
-                key=f'generation-split-{split}-step-{trainer.global_step}-epoch-{trainer.current_epoch}',
-                columns=list(generated[0].keys()),
-                data=[list(d.values()) for d in generated]
-            )
+            for data in generated:
+                data_values = [data[col.value] for col in TableColumns]
+                self.table.add_data(*data_values)
+
+            table_name = f'generation-split-{split}-epoch-{trainer.current_epoch}-step-{trainer.global_step}'
+            wandb.log({table_name: self.table}, commit=True)
 
             print(f'\n{"=" * 25} Generated text from {split.upper()} set {"=" * 25}')
             for x in generated:
