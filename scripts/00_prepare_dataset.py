f6b0daae908d6274bd520040d93751ad9ee4b44b
gianlucagiudice <gianlu.giudice@gmail.com>
2024-09-16 00:22:53 +0200

    refactor: refactoring

--- scripts/00_prepare_dataset.py
+++ scripts/00_prepare_dataset.py
@@ -1,22 +1,14 @@
 import os
-import random
-import re
-from typing import Optional
 
 import fire
 import torch
 
-from yagpt.dataset import YaDataset
+from yagpt.tokenizer import CharTokenizer
 
 
-def read_dataset(dataset_path: str, lower_case: bool, shuffle_sections: bool) -> str:
+def read_dataset(dataset_path: str, lower_case: bool) -> str:
     with open(dataset_path, 'r') as f:
         text = f.read()
-    if shuffle_sections:
-        sections = re.split(r'\n{3}', text)
-        sections = list(map(str.strip, sections))
-        random.shuffle(sections)
-        text = '\n\n\n'.join(sections)
     if lower_case:
         text = text.lower()
     return text
@@ -28,18 +20,10 @@ def split_dataset(tokens, train_ratio):
     return train_tokens, val_tokens
 
 
-def remap_tokens(tokens):
-    unique_tokens = list(set(tokens))
-    id_to_token = {i: t for i, t in enumerate(unique_tokens)}
-    token_to_id = {t: i for i, t in id_to_token.items()}
-
-    return id_to_token, token_to_id
-
-
 def main(
         dataset_path: str,
         output_dir: str = None,
-        train_ratio: float = 0.9
+        train_ratio: float = 0.8
 ):
     if dataset_path is None:
         raise ValueError('dataset_path must be provided')
@@ -47,17 +31,17 @@ def main(
         output_dir = os.path.dirname(dataset_path)
 
     # Read dataset
-    raw_text = read_dataset(dataset_path, lower_case=False, shuffle_sections=True)
+    raw_text = read_dataset(dataset_path, lower_case=False)
     # Tokenize dataset
-    tokens = YaDataset.tokenizer.encode(raw_text)
+    char_tokenizer = CharTokenizer()
+    tokens = char_tokenizer.encode(raw_text)
     # Remap tokens
-    id_to_token, token_to_id = remap_tokens(tokens)
-    tokens = [token_to_id[t] for t in tokens]
+    tokenizer_mapping = char_tokenizer.tokenizer_mapping
+    id_to_token, token_to_id = tokenizer_mapping.id_to_token, tokenizer_mapping.token_to_id
     # Split dataset
     train_tokens, val_tokens = split_dataset(tokens, train_ratio=train_ratio)
     # Save dataset
     os.makedirs(output_dir, exist_ok=True)
-
     train_data = dict(tokens=train_tokens, id_to_token=id_to_token, token_to_id=token_to_id)
     torch.save(train_data, os.path.join(output_dir, 'train.bin'))
     val_data = dict(tokens=val_tokens, id_to_token=id_to_token, token_to_id=token_to_id)
