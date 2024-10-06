9be4dfb10546e636b3684afd279d8a6aca667634
gianlucagiudice <gianlu.giudice@gmail.com>
2024-10-06 23:48:13 +0200

    feat: specify tokenizer

--- scripts/00_prepare_dataset.py
+++ scripts/00_prepare_dataset.py
@@ -3,7 +3,7 @@ import os
 import fire
 import torch
 
-from yagpt.tokenizer import CharTokenizer
+from yagpt.tokenizer import tokenizer_factory
 
 
 def read_dataset(dataset_path: str, lower_case: bool) -> str:
@@ -22,30 +22,36 @@ def split_dataset(tokens, train_ratio):
 
 def main(
         dataset_path: str,
+        tokenizer_name: str = 'bpe',
+        vocab_size: int = 1_000,
         output_dir: str = None,
-        train_ratio: float = 0.8
+        train_ratio: float = 0.8,
+        verbose: bool = True
 ):
     if dataset_path is None:
         raise ValueError('dataset_path must be provided')
     if output_dir is None:
         output_dir = os.path.dirname(dataset_path)
+    os.makedirs(output_dir, exist_ok=True)
 
     # Read dataset
     raw_text = read_dataset(dataset_path, lower_case=False)
+    # Initialize tokenizer
+    tokenizer = tokenizer_factory(tokenizer_name)
+    # Train tokenizer
+    tokenizer.train(
+        text=raw_text,
+        vocab_size=vocab_size,
+        verbose=verbose,
+        save_path=os.path.join(output_dir, 'tokenizer')
+    )
     # Tokenize dataset
-    char_tokenizer = CharTokenizer()
-    tokens = char_tokenizer.encode(raw_text)
-    # Remap tokens
-    tokenizer_mapping = char_tokenizer.tokenizer_mapping
-    id_to_token, token_to_id = tokenizer_mapping.id_to_token, tokenizer_mapping.token_to_id
+    tokens = tokenizer.encode(raw_text)
     # Split dataset
     train_tokens, val_tokens = split_dataset(tokens, train_ratio=train_ratio)
     # Save dataset
-    os.makedirs(output_dir, exist_ok=True)
-    train_data = dict(tokens=train_tokens, id_to_token=id_to_token, token_to_id=token_to_id)
-    torch.save(train_data, os.path.join(output_dir, 'train.bin'))
-    val_data = dict(tokens=val_tokens, id_to_token=id_to_token, token_to_id=token_to_id)
-    torch.save(val_data, os.path.join(output_dir, 'val.bin'))
+    torch.save(train_tokens, os.path.join(output_dir, 'train.bin'))
+    torch.save(val_tokens, os.path.join(output_dir, 'val.bin'))
 
 
 if __name__ == '__main__':
