185f3c646f53d64be0bf1d1899334456d00e4c66
gianlucagiudice <gianlu.giudice@gmail.com>
2024-10-06 23:47:23 +0200

    refactor: char tokenizer

--- yagpt/tokenizer/char_tokenizer.py
+++ yagpt/tokenizer/char_tokenizer.py
@@ -1,7 +1,29 @@
-from typing import List
+from dataclasses import dataclass
+from typing import List, Union
 
 from yagpt.tokenizer import AbstractTokenizer
-from yagpt.tokenizer import TokenizerMapping
+
+
+@dataclass
+class TokenizerMapping:
+    id_to_token: dict[int, str]
+    token_to_id: dict[str, int]
+
+    def __len__(self):
+        return len(self.id_to_token)
+
+    def __getitem__(self, item: Union[int, str]) -> Union[int, str]:
+        if isinstance(item, int):
+            return self.id_to_token[item]
+        if isinstance(item, str):
+            return self.token_to_id[item]
+        raise ValueError("item must be either int or str")
+
+    def __str__(self) -> str:
+        out_str = "TokenizerMapping:\n"
+        for i, t in self.id_to_token.items():
+            out_str += f"({i}: {t.encode()}) "
+        return out_str
 
 
 def init_vocabulary() -> TokenizerMapping:
